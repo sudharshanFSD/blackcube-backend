c318050b4e58c17f0175701305ad3d437be3c11b
sudharshanFSD <tsrssudharshan>
2026-06-25 20:00:42 +0530

    Fix authentication cookie for deployment

--- controllers/authController.js
+++ controllers/authController.js
@@ -65,10 +65,12 @@ const login = async (req, res) => {
       }
     );
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "None",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
 
     res.status(200).json({
       message: "Login Successful",

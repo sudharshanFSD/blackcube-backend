de9a7657492ee9b03aeed816ae4122af8f187ccb
sudharshanFSD <tsrssudharshan>
2026-06-25 20:19:20 +0530

    Fix Netlify CORS origin

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app=express();
 app.use(
   cors({
     origin: ["http://localhost:5173",
-      "https://blackcubemern.netlify.app/"
+      "https://blackcubemern.netlify.app"
     ],
     credentials: true,
   })
